805579549613d9175fb760a1354b5790d6ba8516
Sitting Fox <SittingFox@users.noreply.github.com>
2015-05-07 23:12:15 -0400

    feat: Add data for different categories in JSONs
    Create JSONs for bio, education, work, and projects.

---
+++ js/resumeBuilder.js
@@ -0,0 +1,64 @@
+var bio = {
+	"name": "Ash Ketchum",
+	"role": "Pokémon Trainer",
+	"contacts": {
+		"mobile": "(150) 151-1000",
+		"email": "aketchum@émail.com",
+		"github": "MAshter",
+		"twitter": "@GottaKetchumAll",
+		"location": "Setagaya, Tokyo"
+	},
+	"welcomeMessage": "Welcome to my page!",
+	"skills": ["training", "hardworking", "surviving Thundershocks"],
+	"biopic": "http://placekitten.com/g/150/150",
+	"display": null
+};
+
+var education = {
+	"schools": [
+		{
+			"name": "Pokémon Technical Institute",
+			"location": "Setagaya, Tokyo",
+			"degree": "none",
+			"majors": ["training"],
+			"dates": 1997,
+			"url": "http://bulbapedia.bulbagarden.net"
+		}
+	],
+	"onlineCourses": [
+		{
+		"title": "JavaScript Basics",
+		"school": "Udacity",
+		"date": 2015,
+		"url": "http://udacity.com"
+		}
+	],
+	"display": null
+};
+
+var work = {
+	"jobs": [
+		{
+			"employer": "Self-employed",
+			"title": "Pokémon Trainer",
+			"location": "Tokyo, Japan,",
+			"dates": "1997-present",
+			"description": "Traveling around the world, collecting and training Pokémon."
+		}
+	],
+	"display": null
+};
+
+var projects = {
+	"projects": [
+		{
+			"title": "PokéDex",
+			"dates": "1997-present",
+			"descriptions": "An index on the many different Pokémon.",
+			"images": [
+				"http://placekitten.com/g/200/200"
+			]
+		}
+	],
+	"display": null
+}
